2a911da28e5a6015a695055ace9bf7ed400ed868
DeonFranklin <deondeefranklin@gmail.com>
2022-02-16 15:39:44 -0800

    inital commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>HolÀ Mundo</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App

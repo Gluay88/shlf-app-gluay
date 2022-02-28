3e8423794d3f719bd66c4849374dddf32c4f07c0
yannsf29 <yann.crennsf@gmail.com>
2022-02-28 14:24:08 -0800

    Heroku

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
-
+environment.loaders.get('babel').exclude = [/\.test\.js$/, environment.loaders.get('babel').exclude]
 module.exports = environment.toWebpackConfig()

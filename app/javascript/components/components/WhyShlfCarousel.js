874f70f975bc36c5d8692779ffa67fa5af87495c
yannsf29 <yann.crennsf@gmail.com>
2022-02-28 15:57:17 -0800

    heroku

--- app/javascript/components/components/WhyShlfCarousel.js
+++ app/javascript/components/components/WhyShlfCarousel.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import images from "../assets/fruit.png";
 
 const WhyShlfCarousel = () => {
